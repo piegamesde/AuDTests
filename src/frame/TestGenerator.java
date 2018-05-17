44ad9953d22008f0ddd9323bc94562c0da00f9f8
Piegames <14054505+piegamesde@users.noreply.github.com>
2018-05-17 22:53:24 +0200

    TestGenerator reuses old keys

--- src/frame/TestGenerator.java
+++ src/frame/TestGenerator.java
@@ -5,16 +5,19 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import lab.HashTable;
 
 public class TestGenerator {
 
 	public static void main(String[] args) throws IOException {
+		// if (true)
+		// return;
+		// ;
 		{
 			System.out.println("Hashes");
-			List<String> keys = IntStream.range(0, 10000).mapToObj(i -> randomKey(9)).collect(Collectors.toList());
-			Files.write(Paths.get("TestFile3"), keys);
+			List<String> keys = Files.readAllLines(Paths.get("TestFile3"));
+			// List<String> keys = IntStream.range(0, 10000).mapToObj(i -> randomKey(9)).collect(Collectors.toList());
+			// Files.write(Paths.get("TestFile3"), keys);
 			for (int i = 0; i < 5; i++) {
 				System.out.println(i + "-1");
 				generateHashes(keys, i, HashTable.HASH_FUNCTION_DIVISION);
@@ -26,8 +29,9 @@ public class TestGenerator {
 		}
 		{
 			System.out.println("Dot files");
-			List<String> keys = IntStream.range(0, 10).mapToObj(i -> randomKey(5) + ";" + randomKey(4) + ";" + randomKey(2)).collect(Collectors.toList());
-			Files.write(Paths.get("TestFile4"), keys);
+			List<String> keys = Files.readAllLines(Paths.get("TestFile4"));
+			// List<String> keys = IntStream.range(0, 10).mapToObj(i -> randomKey(5) + ";" + randomKey(4) + ";" + randomKey(2)).collect(Collectors.toList());
+			// Files.write(Paths.get("TestFile4"), keys);
 			generateDot(keys, 4, HashTable.HASH_FUNCTION_DIVISION, HashTable.COLLISION_RESOLUTION_LINEARPROBING);
 			generateDot(keys, 4, HashTable.HASH_FUNCTION_DIVISION, HashTable.COLLISION_RESOLUTION_QUADRATICPROBING);
 			generateDot(keys, 4, HashTable.HASH_FUNCTION_FOLDING, HashTable.COLLISION_RESOLUTION_LINEARPROBING);
@@ -37,8 +41,9 @@ public class TestGenerator {
 		}
 		{
 			System.out.println("Dot files");
-			List<String> keys = IntStream.range(0, 10000).mapToObj(i -> randomKey(5) + ";" + randomKey(4) + ";" + randomKey(2)).collect(Collectors.toList());
-			Files.write(Paths.get("TestFile5"), keys);
+			List<String> keys = Files.readAllLines(Paths.get("TestFile5"));
+			// List<String> keys = IntStream.range(0, 10000).mapToObj(i -> randomKey(5) + ";" + randomKey(4) + ";" + randomKey(2)).collect(Collectors.toList());
+			// Files.write(Paths.get("TestFile5"), keys);
 			generateDot(keys, 5, HashTable.HASH_FUNCTION_DIVISION, HashTable.COLLISION_RESOLUTION_LINEARPROBING);
 			generateDot(keys, 5, HashTable.HASH_FUNCTION_DIVISION, HashTable.COLLISION_RESOLUTION_QUADRATICPROBING);
 			generateDot(keys, 5, HashTable.HASH_FUNCTION_FOLDING, HashTable.COLLISION_RESOLUTION_LINEARPROBING);
