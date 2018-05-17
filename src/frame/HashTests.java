292d9ac699d9be965a455310f440419799e14ca8
Piegames <14054505+piegamesde@users.noreply.github.com>
2018-05-18 00:05:59 +0200

    New test

--- src/frame/HashTests.java
+++ src/frame/HashTests.java
@@ -11,4 +11,19 @@ public class HashTests {
 		HashTable table = new HashTable(800, HashTable.HASH_FUNCTION_FOLDING, "linear_probing");
 		assertEquals(647, table.hash("Z8IG4LDXS"));
 	}
+
+	@Test
+	public void simpleHashTest2() {
+		Entry testEntry1 = new Entry();
+		testEntry1.setKey("Z8IG4LDXS");
+		testEntry1.setData("OK");
+
+		Entry testEntry2 = new Entry();
+		testEntry2.setKey("Z7IG5LDXS");
+		testEntry2.setData("OK");
+
+		HashTable table = new HashTable(10, "folding", "linear_probing");
+		assertEquals(5, table.hash("Z8IG4LDXS"));
+		assertEquals(5, table.hash("Z7IG5LDXS"));
+	}
 }
